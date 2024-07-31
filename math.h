02157591fd14c4d96f1d71244de49ba88c5ce48d
RaresRacsan <82086767+RaresRacsan@users.noreply.github.com>
2024-07-31 01:59:17 -0700

    Update math.h

--- math.h
+++ math.h
@@ -1,18 +1,6 @@
 #ifndef MATH_H
 #define MATH_H
 
-typedef struct {
-    int quotient;
-    int remainder;
-} Division;
-
-// Declaration of the functions
-extern int add_numbers(int a, int b);
-extern int sub_numbers(int a, int b);
-extern int mul_numbers(int a, int b);
-extern Division div_numbers(int a, int b);
-extern int _abs(int a);
-
 extern double cos_function(double a);
 extern double sin_function(double a);
 extern double tan_function(double a);
@@ -26,7 +14,9 @@ extern int is_lessgreater(int a, int b);
 extern double fdim(double a, double b);
 extern double fmax(double a, double b);
 extern double fmin(double a, double b);
+
 extern double fabs(double a);
+extern int _abs(int a);
 extern double fma(double a, double b, double c);
 
 extern int _pow(int a, int b);
